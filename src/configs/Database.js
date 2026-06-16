22cf4015bb839108d35a8a786360111be9c533be
NATHALIA MODESTO NEVES <nathalia.neves10@senaisp.edu.br>
2026-06-16 12:20:48 -0300

    corrigindo identação e server.js

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -57,58 +57,58 @@ export async function initializeDatabase() {
 
 
         await tempConnection.query(`
-           CREATE TABLE IF NOT EXISTS Categorias (
-    IdCategoria INT AUTO_INCREMENT PRIMARY KEY,
-    NomeCategoria VARCHAR(100) NOT NULL,
-    Descricao VARCHAR(255),
-    DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+            CREATE TABLE IF NOT EXISTS Categorias (
+            IdCategoria INT AUTO_INCREMENT PRIMARY KEY,
+            NomeCategoria VARCHAR(100) NOT NULL,
+            Descricao VARCHAR(255),
+            DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
         `);
 
+
+            await tempConnection.query(`
+            CREATE TABLE IF NOT EXISTS Produtos (
+                IdProduto INT AUTO_INCREMENT PRIMARY KEY,
+                IdCategoria INT NOT NULL,
+                NomeProduto VARCHAR(150) NOT NULL,
+                DescricaoProduto VARCHAR(255),
+                Preco DECIMAL(10,2) NOT NULL,
+                Imagem VARCHAR(255),
+                Estoque INT NOT NULL DEFAULT 0,
+                DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+            CONSTRAINT FK_Produtos_Categorias
+                FOREIGN KEY (IdCategoria)
+                REFERENCES Categorias(IdCategoria)
+    );
+        `);
 
         await tempConnection.query(`
-          CREATE TABLE IF NOT EXISTS Produtos (
-    IdProduto INT AUTO_INCREMENT PRIMARY KEY,
-    IdCategoria INT NOT NULL,
-    NomeProduto VARCHAR(150) NOT NULL,
-    DescricaoProduto VARCHAR(255),
-    Preco DECIMAL(10,2) NOT NULL,
-    Imagem VARCHAR(255),
-    Estoque INT NOT NULL DEFAULT 0,
-    DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT FK_Produtos_Categorias
-        FOREIGN KEY (IdCategoria)
-        REFERENCES Categorias(IdCategoria)
+            CREATE TABLE IF NOT EXISTS Pedidos (
+                IdPedido INT AUTO_INCREMENT PRIMARY KEY,
+                ValorTotal DECIMAL(10,2) NOT NULL,
+                StatusPedido ENUM('Aberto','Finalizado','Pendente'),
+                DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
         `);
 
         await tempConnection.query(`
-         CREATE TABLE IF NOT EXISTS Pedidos (
-    IdPedido INT AUTO_INCREMENT PRIMARY KEY,
-    ValorTotal DECIMAL(10,2) NOT NULL,
-    StatusPedido ENUM('Aberto','Finalizado','Pendente'),
-    DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-        `);
+            CREATE TABLE IF NOT EXISTS ItensPedido (
+                IdItemPedido INT AUTO_INCREMENT PRIMARY KEY,
+                IdPedido INT NOT NULL,
+                IdProduto INT NOT NULL,
+                Quantidade INT NOT NULL,
+                PrecoUnitario DECIMAL(10,2) NOT NULL,
+                DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                await tempConnection.query(`
-CREATE TABLE IF NOT EXISTS ItensPedido (
-    IdItemPedido INT AUTO_INCREMENT PRIMARY KEY,
-    IdPedido INT NOT NULL,
-    IdProduto INT NOT NULL,
-    Quantidade INT NOT NULL,
-    PrecoUnitario DECIMAL(10,2) NOT NULL,
-    DataCad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                CONSTRAINT FK_ItensPedido_Pedidos
+                    FOREIGN KEY (IdPedido)
+                    REFERENCES Pedidos(IdPedido),
 
-    CONSTRAINT FK_ItensPedido_Pedidos
-        FOREIGN KEY (IdPedido)
-        REFERENCES Pedidos(IdPedido),
-
-    CONSTRAINT FK_ItensPedido_Produtos
-        FOREIGN KEY (IdProduto)
-        REFERENCES Produtos(IdProduto)
-);
+                CONSTRAINT FK_ItensPedido_Produtos
+                    FOREIGN KEY (IdProduto)
+                    REFERENCES Produtos(IdProduto)
+            );
         `);
 
 
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
-import cors from 'cors';
+import { initializeDatabase } from './configs/Database.js';
+// import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
-app.use('/uploads', express.static('uploads'));
+
+// app.use('/uploads', express.static('uploads'));
+
 app.use(express.json());
 app.use('/', routes);
 
