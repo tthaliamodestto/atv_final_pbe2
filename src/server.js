ccd89f3e454f8bc6ba7d071a4479ba27465581c4
NATHALIA MODESTO NEVES <nathalia.neves10@senaisp.edu.br>
2026-06-16 10:58:02 -0300

    corrigindo server.js

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${process.env.SERVER_PORT}`);
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
 });
