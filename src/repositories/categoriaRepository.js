eb9884cda8c2468c15f8f3ccfa73b4a3be6f2fcd
NATHALIA MODESTO NEVES <nathalia.neves10@senaisp.edu.br>
2026-06-16 13:43:54 -0300

    corrigindo funcoes

--- src/repositories/categoriaRepository.js
+++ src/repositories/categoriaRepository.js
@@ -2,14 +2,14 @@ import { connection } from "../configs/Database.js";
 
 const categoriaRepository = {
     criar: async (categoria) => {
-        const sql = 'INSERT INTO categorias (NomeCategoria, DescricaoCategoria) VALUES (?, ?);';
+        const sql = 'INSERT INTO categorias (NomeCategoria, Descricao) VALUES (?, ?);';
         const values = [categoria.nome, categoria.descricao];
         const [rows] = await connection.execute(sql, values);
         return rows;
     },
     
     editar: async (categoria) => {
-        const sql = 'UPDATE categorias SET NomeCategoria = ?, DescricaoCategoria = ? WHERE IdCategoria = ?;';
+        const sql = 'UPDATE categorias SET NomeCategoria = ?, Descricao = ? WHERE IdCategoria = ?;';
         const values = [categoria.nome, categoria.descricao, categoria.idCategoria];
         const [rows] = await connection.execute(sql, values);
         return rows;
@@ -22,7 +22,7 @@ const categoriaRepository = {
     },
     
     selecionar: async () => {
-        const sql = 'SELECT IdCategoria, NomeCategoria, DescricaoCategoria FROM categorias;';
+        const sql = 'SELECT IdCategoria, NomeCategoria, Descricao FROM categorias;';
         const [rows] = await connection.execute(sql);
         return rows;
     }
